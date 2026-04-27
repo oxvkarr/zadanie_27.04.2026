403ee81fd35e79d3fd745b859d7bb4e5cc923378
Oskar Kurasz <oskarkurasz6@gmail.com>
2026-04-27 09:25:44 +0200

    Tworzy dwie wersje tej samej metody

---
+++ PrzeciazenieMetody.java
@@ -0,0 +1,14 @@
+public class PrzeciazenieMetody {
+
+  public int dodaj(int a, int b) {
+    return a + b;
+  }
+
+  public int dodaj(int a, int b, int c) {
+    return a + b + c;
+  }
+
+  public double dodaj(double a, double b) {
+    return a + b;
+  }
+}
